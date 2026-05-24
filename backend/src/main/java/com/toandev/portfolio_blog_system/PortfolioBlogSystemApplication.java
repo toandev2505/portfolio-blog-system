7fc10e7c356f6452b615988fb68da9ef4a8d5b3c
Toan Dev <toandev2505@gmail.com>
2026-05-24 13:19:17 +0700

    reload backend folder

---
+++ backend/src/main/java/com/toandev/portfolio_blog_system/PortfolioBlogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.toandev.portfolio_blog_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBlogSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBlogSystemApplication.class, args);
+	}
+
+}
