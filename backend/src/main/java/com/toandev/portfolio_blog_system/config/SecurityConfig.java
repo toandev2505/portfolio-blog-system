2028147feef6bb44a6b64cb5c36f9952e6b7c19a
Toan Truong <toandev2505@gmail.com>
2026-06-06 21:03:02 +0700

    Add new allowed origin for CORS configuration

--- backend/src/main/java/com/toandev/portfolio_blog_system/config/SecurityConfig.java
+++ backend/src/main/java/com/toandev/portfolio_blog_system/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://leewithcode.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
         configuration.setAllowCredentials(true);
