62ac89bae355515b191128cc3918dbb48920cf17
Toan Truong <toandev2505@gmail.com>
2026-06-06 20:57:27 +0700

    Update axios base URL to production environment

--- frontend/src/api/axiosConfig.jsx
+++ frontend/src/api/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080/api', // Đường dẫn gốc API Backend của bạn
+  baseURL: 'https://leewithcode.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
